e11aa9e249f11aabb6d32a0ae41c21aabb834c98
Minisungam <minisungam@gmail.com>
2022-03-26 00:59:55 -0400

    add hover effects, modal work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Christopher Magnus
-$(document).ready(function(){
+
+$(document).ready(function() {
+
+    // Get current year, add to footer.
     const today = new Date()
 
     document.getElementById("footerText").innerHTML = "&copy;" + today.getFullYear().toString() + " Christopher Magnus";
@@ -9,6 +12,17 @@ $(document).ready(function(){
     var navbar = document.querySelector("nav");
     var content = document.querySelector("#skills");
     var navPos = navbar.offsetTop;
+
+    if (window.pageYOffset >= navPos) 
+    {
+        navbar.classList.add("sticky");
+        content.classList.add("stickyContent");
+    } 
+    else 
+    {
+        navbar.classList.remove("sticky");
+        content.classList.remove("stickyContent");
+    }
 
     function stickyNavbar() 
     {
@@ -24,3 +38,11 @@ $(document).ready(function(){
         }
     }
 })
+
+// Modals
+
+const modalCalculator = document.querySelector("#modalCalculator");
+const modalPatientNotes = document.querySelector("#modalPatientNotes");
+const modalCar = document.querySelector("#modalCar");
+
+$("#modalCalculatorBox").click(function() { modalCalculator.showModal() })
