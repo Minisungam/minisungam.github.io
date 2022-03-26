0e770137da0dcade07efdb7fec9101e9be27ad24
Minisungam <minisungam@gmail.com>
2022-03-25 22:16:40 -0400

    expand content areas, add header background, more formatting

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-const today = new Date()
+// Christopher Magnus
+$(document).ready(function(){
+    const today = new Date()
 
-function onLoad()
-{
     document.getElementById("footerText").innerHTML = "&copy;" + today.getFullYear().toString() + " Christopher Magnus";
-}
+
+    // Sticky Navbar
+    window.onscroll = function() { stickyNavbar() };
+    var navbar = document.querySelector("nav");
+    var skills = document.querySelector("main")
+    var navPos = navbar.offsetTop;
+
+    function stickyNavbar() 
+    {
+        if (window.pageYOffset >= navPos) 
+        {
+            navbar.classList.add("sticky")
+            skills.classList.add("stickyContent")
+        } 
+        else 
+        {
+            navbar.classList.remove("sticky");
+            skills.classList.remove("stickyContent")
+        }
+    }
+})
